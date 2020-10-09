0efe28a966840455597b926ff9757ef6bc88e1a8
ojasbi7 <70551107+ojasbi7@users.noreply.github.com>
2020-10-08 23:37:51 -0400

    Update Assignment 2.js

--- Assignment 2.js
+++ Assignment 2.js
@@ -2,3 +2,27 @@ document.addEventList('DOMContentLoaded', function() {
 
 document.querySelector("#new-task").onsubmit = function() {
 
+
+button.remove:hover{background-color: #FF4F55;
+
+}
+
+input{
+
+  cursor: pointer;
+
+}
+
+input:focus {
+  cursor: pointer;
+  outline: none;
+  background-color: pink;
+}
+
+li {
+  text-align: left;
+  font-size: 15px;
+  font-style: normal;
+  list-style: none;
+  margin: 0;
+}
