58fffbfe04fcc812416caa4aaf153f9ee060ca42
ojasbi7 <70551107+ojasbi7@users.noreply.github.com>
2020-10-09 13:05:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-document.addEventList('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function(){
 
-document.querySelector("#new-task").onsubmit = function() {
+  document.querySelector("#new-task").onsubmit= function(){
+
+    const li=document.createElement('li');
+    let task_text= document.querySelector("#task").value;
+    let new_task_html = `
+                            <span> ${task_text} </span>
+                            <button class= "complete"> Completed </button>
+                            <button class="remove"> Remove </button>
 
